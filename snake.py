30f3edf8223b1ec36e95fa759ff0847cae3289b1
Richard Cross <97173980+Trickaay@users.noreply.github.com>
2025-01-13 17:22:45 +0000

    Updated snake.py
    Setup initial game loop

--- snake.py
+++ snake.py
@@ -1,5 +1,4 @@
-import pygame
-
+import pygame, sys
 
 pygame.init()
 
@@ -8,3 +7,12 @@ screen = pygame.display.set_mode((800, 600))
 pygame.display.set_caption("Snake Game")
 
 clock = pygame.time.Clock()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+    
+    pygame.display.update()
+    clock.tick(60)
