95ee80efc9042c08f53ec9bafcd2e31b79ae30d8
Richard Cross <97173980+Trickaay@users.noreply.github.com>
2025-01-13 17:27:36 +0000

    Update snake.py
    Setting the display size via grid

--- snake.py
+++ snake.py
@@ -2,7 +2,13 @@ import pygame, sys
 
 pygame.init()
 
-screen = pygame.display.set_mode((800, 600))
+GREEN = (173, 204, 96)
+DARK_GREEN = (43, 51, 24)
+
+cell_size = 30
+number_of_cells = 25
+
+screen = pygame.display.set_mode((cell_size*number_of_cells, cell_size*number_of_cells))
 
 pygame.display.set_caption("Snake Game")
 
@@ -14,5 +20,6 @@ while True:
             pygame.quit()
             sys.exit()
     
+    screen.fill(GREEN)
     pygame.display.update()
     clock.tick(60)
